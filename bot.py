bda40fc35fc340af357c66eb6b060adaa8785178
dromzeh <65048232+dromzeh@users.noreply.github.com>
2020-12-04 14:57:52 +0000

    Update bot.py
    made stuff neater for once - lgtm

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ TOKEN = 'YOUR_TOKEN_HERE' # ENTER YOUR TOKEN HERE
 GAME_NAME = 'Example Game' # WRITE WHAT YOU WANT YOUR BOT TO BE PLAYING
 
 bot = commands.Bot(command_prefix=prefix1)
-bot.remove_command('help')
+bot.remove_command('help') # DELETES ORIGINAL HELP COMMAND TO REPLACE WITH CUSTOM EMBED
 
 @bot.event
 async def on_ready():
@@ -24,7 +24,7 @@ async def ping(ctx):
     t_1 = time.perf_counter()
     await ctx.trigger_typing()
     t_2 = time.perf_counter()
-    ms = round((t_2-t_1)*1000)
+    ms = round((t_2-t_1)*1000) # grabs ping and rounds it so it's not a stupid number
     embed = discord.Embed(color=0xff0000)
     embed.add_field(name=":ping_pong: Pong!", value = f"{ms}ms")
     await ctx.send(embed=embed)
@@ -39,16 +39,7 @@ async def help(ctx):
     embed.add_field(name=f"{bot.user.name}'s Commands List", value = "`ping` `help` `invite` `ban` `kick` `choose` `jointime` `8ball`")
     await ctx.send(embed=embed)
 
-@bot.command()
-@has_permissions(ban_members=True)
-async def ban(ctx, member: discord.Member, *, reason: str = None):
-    if reason == None:
-        reason = 'No Reason Provided'
-    await member.kick(reason=reason)
-    embed = discord.Embed(color=0x0000ff)
-    embed.add_field(name=f"{user} has been banned by {ctx.message.author}", value = f"`{reason}`")
-    await ctx.send(embed=embed)
-
+ 
 @bot.command()
 @has_permissions(kick_members=True)
 async def kick(ctx, member: discord.Member, *, reason: str = None):
@@ -60,16 +51,26 @@ async def kick(ctx, member: discord.Member, *, reason: str = None):
     await ctx.send(embed=embed)
 
 @bot.command()
-async def choose(ctx, *choice: str):
-    await ctx.send(f"{ctx.message.author} - I choose **{choice}**!")
-
+@has_permissions(ban_members=True)
+async def ban(ctx, member: discord.Member, *, reason: str = None):
+    if reason == None:
+        reason = 'No Reason Provided'
+    await member.kick(reason=reason)
+    embed = discord.Embed(color=0x0000ff)
+    embed.add_field(name=f"{user} has been banned by {ctx.message.author}", value = f"`{reason}`")
+    await ctx.send(embed=embed)
+    
 @bot.command()
 @guild_only()
 async def jointime(ctx, member: discord.Member = None):
     if member == None:
         member = ctx.message.author
     await ctx.send(f'{member.name} joined at `{member.joined_at}`')
-
+    
+@bot.command()
+async def choose(ctx, *choice: str):
+    await ctx.send(f"{ctx.message.author} - I choose **{choice}**!")
+    
 @bot.command(aliases=["8ball"])
 async def eightball(ctx):
     responses_list = ['Yes.', 'No.', 'Maybe.', 'Definitely', 'Not at all.', 'Ask me another time.']
